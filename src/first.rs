1b26529f42f04032f02cf6743bafa4f42ef7f700
Fumiya Fujinaka <uzuna.kf@gmail.com>
2019-12-01 06:58:07 +0900

    add function symbol

---
+++ src/first.rs
@@ -0,0 +1,11 @@
+pub fn sort(x: &mut [u32], up: bool) {
+  unimplemented!();
+}
+
+pub fn sub_sort(x: &mut [u32], up: bool) {
+  unimplemented!();
+}
+
+pub fn compare_and_swap(x: &mut [u32], up: bool) {
+  unimplemented!();
+}
