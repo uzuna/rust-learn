7be8fafb44277fc8b2b047cdf9c103e52d1b9cbd
Fumiya Fujinaka <uzuna.kf@gmail.com>
2019-12-22 09:38:39 +0900

    add std ops range

--- src/primitive/user.rs
+++ src/primitive/user.rs
@@ -6,7 +6,7 @@
 
 #[cfg(test)]
 mod tests {
-
+  use std::collections::HashMap;
   #[test]
   fn r#box() {
     // stackに置かれる
@@ -44,5 +44,99 @@ mod tests {
     v6.extend_from_slice(&a8);
     assert_eq!(v6, ['a', 'f', 'c', 'd', 'g', 'h', 'i', 'j']);
     assert_eq!(a8, ['i', 'j']);
+  }
+
+  #[test]
+  fn collections() {
+    let mut m1 = HashMap::new();
+
+    m1.insert("a", 1);
+    m1.insert("b", 3);
+    assert_eq!(m1.len(), 2);
+
+    assert_eq!(m1.get("b"), Some(&3));
+    assert_eq!(m1.get("c"), None);
+
+    // 無ければ追加するメソッドで参照を得る
+    let d = m1.entry("d").or_insert(0);
+    *d += 7;
+    assert_eq!(m1.get("d"), Some(&7));
+
+    // 配列を使った初期化
+    let m2 = vec![("a", 1), ("b", 2)]
+      .into_iter()
+      .collect::<HashMap<_, _>>();
+
+    assert_eq!(m2.get("b"), Some(&2));
+  }
+
+  #[test]
+  fn type_string() {
+    let mut s1 = "ラズベリー".to_string();
+    let mut s2 = String::from("ブラックベリー");
+
+    s1.push_str("タルト");
+    assert_eq!(s1, "ラズベリータルト");
+
+    s1.push('と');
+    s1.push_str(&s2); //&strしか受け付けないので&をつけて型強制
+    assert_eq!(s1, "ラズベリータルトとブラックベリー");
+
+    let i = 42;
+    assert_eq!(i.to_string(), "42");
+
+    let f = 4.3 + 0.1;
+    assert_eq!(f.to_string(), "4.3999999999999995");
+    assert_eq!(format!("{:.2}", f), "4.40");
+    let s1 = "44";
+    assert_eq!(s1.parse::<i32>(), Ok(44));
+
+    // 解釈できない場合はエラーとなる
+    let s2 = "abc";
+    let r2: Result<f64, _> = s2.parse();
+    assert!(r2.is_err());
+    println!("{:?}", r2);
+
+    // from char
+    let cs = ['t', 'r', 'u', 's', 't'];
+    assert_eq!(cs.iter().collect::<String>(), "trust");
+    assert_eq!(&cs[1..].iter().collect::<String>(), "rust");
+
+    let bad_utf8: [u8; 7] = [
+      b'a', // a
+      0xf0, 0x90, 0x80, // でたらめなバイト列
+      0xe3, 0x81, 0x82, // あ
+    ];
+    let s = String::from_utf8_lossy(&bad_utf8);
+    assert_eq!(s, "a\u{fffd}あ"); // 不正なバイト列はfffd ReplacementCharacterにおきかわる
+
+    // &strは生存期間の関係で返せない
+    // Stringを返すのが一般的
+    fn gen_string(name: &str) -> String {
+      format!("Hello {}!", name)
+    }
+    assert_eq!(gen_string("world"), "Hello world!");
+
+    // 他OSStringやCStrがある。
+    // 状況に合わせて使う
+  }
+
+  #[test]
+  fn std_ops_range() {
+    // 範囲指定
+    let a = ['a', 'b', 'c', 'd', 'e'];
+
+    assert_eq!(a[..], ['a', 'b', 'c', 'd', 'e']);
+    assert_eq!(a[..3], ['a', 'b', 'c',]);
+    assert_eq!(a[..=3], ['a', 'b', 'c', 'd',]);
+    assert_eq!(a[1..], ['b', 'c', 'd', 'e']);
+    assert_eq!(a[1..3], ['b', 'c',]);
+
+    assert_eq!(.., std::ops::RangeFull);
+    assert_eq!(..3, std::ops::RangeTo { end: 3 });
+    assert_eq!(..=3, std::ops::RangeToInclusive { end: 3 });
+    assert_eq!(1.., std::ops::RangeFrom { start: 1 });
+    assert_eq!(1..3, std::ops::Range { start: 1, end: 3 });
+    assert_eq!(1..=3, std::ops::RangeInclusive::new(1, 3));
   }
 }
