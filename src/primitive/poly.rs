30ed9a2408b41a93e0f94b50f4f87a7c84f32b37
Fumiya Fujinaka <uzuna.kf@gmail.com>
2019-12-31 16:17:22 +0900

    traitの継承

--- src/primitive/poly.rs
+++ src/primitive/poly.rs
@@ -12,6 +12,14 @@ struct PolarCood {
   theta: f64,
 }
 
+struct Matrix([[f64; 2]; 2]);
+
+// traitの継承
+// Coordinatesをすべて実装しているならLinucerTransformの実装を書いてもよいという指示
+trait LinerTrasform: Coordinates {
+  fn transform(self, matrix: &matrix) -> Self;
+}
+
 // traitの基本
 // デカルト座標と相互変換できるものは座標トレイトを持つと定義
 trait Coodinates {
@@ -26,6 +34,19 @@ impl Coodinates for CartesianCoord {
   }
   fn from_catesian(cart: CartesianCoord) -> Self {
     cart
+  }
+}
+
+// Coodinatesを実装していない場合はエラーになる
+impl LinerTrasform for CartesianCoord {
+  fn transform(mut self, matrix: &Matrix) -> Self {
+    let x = self.x;
+    let y = self.y;
+    let m = matrix.0;
+
+    self.x = m[0][0] * x + m[0][1] * y;
+    self.y = m[1][0] * x + m[1][1] * y;
+    self
   }
 }
 
@@ -44,12 +65,53 @@ impl Coodinates for PolarCood {
   }
 }
 
+// tupleにも実装できる
+impl Coodinates for (f64, f64) {
+  fn to_cartesian(self) -> CartesianCoord {
+    CartesianCoord {
+      x: self.0,
+      y: self.1,
+    }
+  }
+  fn from_catesian(cart: CartesianCoord) -> Self {
+    (cart.x, cart.y)
+  }
+}
+
+// Genericsで書いた場合はそのメソッドを持っていることを保証する境界が必要になる
+// fn print_point<P>(point: P) {
+//   let p = point.to_cartesian(); // no methoad name
+//   println!("({}, {})", p.x, p.y);
+// }
+// Trait構文
+fn print_point1<P: Coodinates>(point: P) {
+  let p = point.to_cartesian(); // no methoad name
+  println!("({}, {})", p.x, p.y);
+}
+// inline記法
+fn print_point2<P>(point: P)
+where
+  P: Coodinates,
+{
+  let p = point.to_cartesian(); // no methoad name
+  println!("({}, {})", p.x, p.y);
+}
+// impl Trait構文
+fn print_point3(point: impl Coodinates) {
+  let p = point.to_cartesian(); // no methoad name
+  println!("({}, {})", p.x, p.y);
+}
+
 #[cfg(test)]
 mod tests {
   use crate::primitive::poly::*;
   #[test]
   fn coord() {
-    let c1 = CartesianCoord { x: 0.0, y: 0.0 };
-    assert_eq!(CartesianCoord { x: 0.0, y: 0.0 }, c1.to_cartesian());
+    let p1 = (1.0, 0.0);
+    let c1 = p1.to_cartesian();
+    assert_eq!(CartesianCoord { x: 1.0, y: 0.0 }, c1);
+    let p = PolarCood::from_catesian(c1);
+    assert_eq!(PolarCood { r: 1.0, theta: 0.0 }, p);
+    print_point1(p)
   }
 }
