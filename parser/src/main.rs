e6edd9652ef634120dc046eb8c6742d65700777f
Fumiya Fujinaka <uzuna.kf@gmail.com>
2020-01-05 21:20:29 +0900

    add interpreter

--- parser/src/main.rs
+++ parser/src/main.rs
@@ -400,6 +400,65 @@ where
         })
 }
 
+/// 評価機を表すデータ型
+/// 今回はデータを持たない型。用途によっては評価文脈(context)を持たせる
+struct Interpreter;
+
+impl Interpreter {
+    pub fn new() -> Self {
+        Interpreter
+    }
+    // 評価関数
+    pub fn eval(&mut self, expr: &Ast) -> Result<i64, InterpreterError> {
+        use self::AstKind::*;
+        match expr.value {
+            Num(n) => Ok(n as i64),
+            UniOp { ref op, ref e } => {
+                let e = self.eval(e)?;
+                Ok(self.eval_uniop(op, e))
+            }
+            BinOp {
+                ref op,
+                ref l,
+                ref r,
+            } => {
+                let l = self.eval(l)?;
+                let r = self.eval(r)?;
+                self.eval_binop(op, l, r)
+                    .map_err(|e| InterpreterError::new(e, expr.loc.clone()))
+            }
+        }
+    }
+    fn eval_uniop(&mut self, op: &UniOp, n: i64) -> i64 {
+        use self::UniOpKind::*;
+        match op.value {
+            Plus => n,
+            Minus => -n,
+        }
+    }
+    fn eval_binop(&mut self, op: &BinOp, l: i64, r: i64) -> Result<i64, InterpreterErrorKind> {
+        use self::BinOpKind::*;
+        match op.value {
+            Add => Ok(l + r),
+            Sub => Ok(l - r),
+            Mult => Ok(l * r),
+            Div => {
+                if r == 0 {
+                    Err(InterpreterErrorKind::DivisionByZero)
+                } else {
+                    Ok(l / r)
+                }
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+enum InterpreterErrorKind {
+    DivisionByZero,
+}
+type InterpreterError = Annot<InterpreterErrorKind>;
+
 /// エラー処理を定義
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 enum Error {
@@ -463,6 +522,26 @@ impl fmt::Display for ParseError {
             ),
             Eof => write!(f, "End of file"),
         }
+    }
+}
+
+impl fmt::Display for InterpreterError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use self::InterpreterErrorKind::*;
+        match self.value {
+            DivisionByZero => write!(f, "division by zero"),
+        }
+    }
+}
+
+impl StdError for InterpreterError {}
+
+impl InterpreterError {
+    fn show_diagnostic(&self, input: &str) {
+        // エラー情報を簡単に表示し
+        eprintln!("{}", self);
+        // エラー位置を指示する
+        print_annot(input, self.loc.clone());
     }
 }
 
@@ -550,6 +629,9 @@ fn prompt(s: &str) -> io::Result<()> {
 fn main() {
     use std::io::{stdin, BufRead, BufReader};
 
+    // interpreter
+    let mut interp = Interpreter::new();
+
     let stdin = stdin();
     let stdin = stdin.lock();
     let stdin = BufReader::new(stdin);
@@ -566,7 +648,16 @@ fn main() {
                     continue;
                 }
             };
-            println!("{:?}", ast);
+            // eval
+            let n = match interp.eval(&ast) {
+                Ok(n) => n,
+                Err(e) => {
+                    e.show_diagnostic(&line);
+                    show_trace(e);
+                    continue;
+                }
+            };
+            println!("{:?}", n);
         } else {
             break;
         }
