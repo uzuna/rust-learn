58af7e4c3737f026f4ae91f5c1817fb658854e8d
Fumiya Fujinaka <uzuna.kf@gmail.com>
2020-01-05 20:29:51 +0900

    impl StdError

--- parser/src/main.rs
+++ parser/src/main.rs
@@ -418,7 +418,76 @@ impl From<ParseError> for Error {
     }
 }
 
+use std::fmt;
+impl fmt::Display for TokenKind {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use self::TokenKind::*;
+        match self {
+            Number(n) => n.fmt(f),
+            Plus => write!(f, "+"),
+            Minus => write!(f, "-"),
+            Asterisk => write!(f, "*"),
+            Slash => write!(f, "/"),
+            LParen => write!(f, "("),
+            RParen => write!(f, ")"),
+        }
+    }
+}
+impl fmt::Display for Loc {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}-{}", self.0, self.1)
+    }
+}
+impl fmt::Display for LexError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use self::LexErrorKind::*;
+        let loc = &self.loc;
+        match self.value {
+            InvalidChar(c) => write!(f, "{}: invalid char '{}'", loc, c),
+            Eof => write!(f, "End of file"),
+        }
+    }
+}
+impl fmt::Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use self::ParseError::*;
+        match self {
+            UnexpectedToken(tok) => write!(f, "{}: {} is not expected", tok.loc, tok.value),
+            NotExpression(tok) => write!(f, "{}: {} is not start expression", tok.loc, tok.value),
+            NotOperator(tok) => write!(f, "{}: {} is not an operator", tok.loc, tok.value),
+            UnclosedOpenParen(tok) => write!(f, "{}: {} is not closed", tok.loc, tok.value),
+            RedundantExpression(tok) => write!(
+                f,
+                "{}: expression after '{}' is redundant",
+                tok.loc, tok.value
+            ),
+            Eof => write!(f, "End of file"),
+        }
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "parser error")
+    }
+}
+
+use std::error::Error as StdError;
+impl StdError for LexError {}
+impl StdError for ParseError {}
+impl StdError for Error {
+    fn source(&self) -> Option<&(dyn StdError + 'static)> {
+        use self::Error::*;
+        match self {
+            Lexer(lex) => Some(lex),
+            Parser(parse) => Some(parse),
+        }
+    }
+}
+
 use std::str::FromStr;
+
+/// parse::<type>() で文字列から直接作るためにFromStrトレイトを実装
 impl FromStr for Ast {
     type Err = Error;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
@@ -452,7 +521,14 @@ fn main() {
             let ast = match line.parse::<Ast>() {
                 Ok(ast) => ast,
                 Err(e) => {
-                    unimplemented!();
+                    eprintln!("{}", e);
+                    // sourceの表示
+                    let mut source = e.source();
+                    while let Some(e) = source {
+                        eprintln!("cause by {}", e);
+                        source = e.source()
+                    }
+                    continue;
                 }
             };
             println!("{:?}", ast);
