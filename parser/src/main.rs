b173e5a4ab1338e4e13cf45d73aabfeaa2c1c830
Fumiya Fujinaka <uzuna.kf@gmail.com>
2020-01-05 11:08:02 +0900

    add lexer interpriter

--- parser/src/main.rs
+++ parser/src/main.rs
@@ -171,7 +171,6 @@ fn recognize_many(input: &[u8], mut pos: usize, mut f: impl FnMut(u8) -> bool) -
 }
 
 #[test]
-
 fn test_lexer() {
     assert_eq!(lex("1"), Ok(vec![Token::number(1, Loc(0, 1))]));
     assert_eq!(
@@ -187,4 +186,33 @@ fn test_lexer() {
             Token::number(10, Loc(13, 15)),
         ])
     );
+}
+
+use std::io;
+
+fn prompt(s: &str) -> io::Result<()> {
+    use std::io::{stdout, Write};
+    let stdout = stdout();
+    let mut stdout = stdout.lock();
+    stdout.write(s.as_bytes())?;
+    stdout.flush()
+}
+
+fn main() {
+    use std::io::{stdin, BufRead, BufReader};
+
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let stdin = BufReader::new(stdin);
+    let mut lines = stdin.lines();
+
+    loop {
+        prompt("> ").unwrap();
+        if let Some(Ok(line)) = lines.next() {
+            let token = lex(&line);
+            println!("{:?}", token);
+        } else {
+            break;
+        }
+    }
 }
