382d3d802d3e09ca7e3d6a7c69c384095b39306c
Fumiya Fujinaka <uzuna.kf@gmail.com>
2020-01-09 23:45:49 +0900

    add staticfile

--- start-aw/src/main.rs
+++ start-aw/src/main.rs
@@ -1,10 +1,14 @@
-use actix_web::{server, App, Error, FromRequest, HttpRequest, Path, Responder};
+use actix_web::{fs, server, App, Error, FromRequest, HttpRequest, Path, Responder, State};
 use serde_derive::*;
 
 #[derive(Deserialize)]
 struct HelloPath {
     // {name}に対するフィールドを定義
     name: String,
+}
+
+struct MyApp {
+    server_name: String,
 }
 
 fn hello(req: &HttpRequest) -> impl Responder {
@@ -16,11 +20,21 @@ fn hello_name(to: Path<HelloPath>) -> impl Responder {
     format!("Hello {}!", to.name)
 }
 
+fn hello_with_state(app: State<MyApp>) -> impl Responder {
+    format!("Hello from {}!", app.server_name)
+}
+
 fn main() {
     server::new(|| {
         App::new()
             .resource("/", |r| r.f(hello))
             .resource("/{name}", |r| r.with(hello_name))
+            .handler("/static", fs::StaticFiles::new(".").unwrap())
+        // with_stateもできるがこの場合はhandlerの型が req:&HttpRequest<MyApp>になって汎用性がなくなる
+        // App::with_state(MyApp {
+        //     server_name: "server with state".into(),
+        // })
+        // .resource("/info", |r| r.with(hello_with_state))
     })
     .bind("localhost:3000")
     .expect("Can not bind to port 3000")
