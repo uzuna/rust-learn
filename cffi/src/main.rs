d5067effac71c3bdf69b70884867adbeb97a628f
Fumiya Fujinaka <uzuna.kf@gmail.com>
2020-01-16 21:24:44 +0900

    link dynamic library

--- cffi/src/main.rs
+++ cffi/src/main.rs
@@ -1,8 +1,12 @@
-use std::os::raw::c_double;
+use std::ffi::{CStr, CString};
+use std::os::raw::{c_double, c_schar};
 
+// sudo apt get install libreadline-dev
+#[link(name = "readline")]
 // インポートするCの関数群を extern C ブロック内に書く
 extern "C" {
     fn cos(x: c_double) -> c_double;
+    fn readline(pronpt: *const c_schar) -> *mut c_schar;
 }
 
 fn main() {
